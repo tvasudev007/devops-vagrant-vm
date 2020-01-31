8f0c0b59e5eb8bac96523e304d2d72907c339887
Interview <Interview@wkmgb4743139.global.publicisgroupe.net>
2020-01-31 17:14:04 +0000

    init

---
+++ scripts/install.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# remove comment if you want to enable debugging
+#set -x
+
+if [ -e /etc/redhat-release ] ; then
+  REDHAT_BASED=true
+fi
+
+TERRAFORM_VERSION="0.12.18"
+PACKER_VERSION="1.2.4"
+# create new ssh key
+[[ ! -f /home/ubuntu/.ssh/mykey ]] \
+&& mkdir -p /home/ubuntu/.ssh \
+&& ssh-keygen -f /home/ubuntu/.ssh/mykey -N '' \
+&& chown -R ubuntu:ubuntu /home/ubuntu/.ssh
+
+# install packages
+if [ ${REDHAT_BASED} ] ; then
+  yum -y update
+  yum install -y docker ansible unzip wget
+else 
+  apt-get update
+  apt-get -y install docker.io ansible unzip
+fi
+# add docker privileges
+usermod -G docker ubuntu
+apt-get update
+apt-get install -y git
+# install pip
+
+pip install -U pip && pip3 install -U pip
+if [[ $? == 127 ]]; then
+    wget -q https://bootstrap.pypa.io/get-pip.py
+    python get-pip.py
+    python3 get-pip.py
+fi
+# install awscli and ebcli
+pip install -U awscli
+pip install -U awsebcli
+
+#terraform
+T_VERSION=$(/usr/local/bin/terraform -v | head -1 | cut -d ' ' -f 2 | tail -c +2)
+T_RETVAL=${PIPESTATUS[0]}
+
+[[ $T_VERSION != $TERRAFORM_VERSION ]] || [[ $T_RETVAL != 0 ]] \
+&& wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+&& unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
+&& rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+# packer
+P_VERSION=$(/usr/local/bin/packer -v)
+P_RETVAL=$?
+
+[[ $P_VERSION != $PACKER_VERSION ]] || [[ $P_RETVAL != 1 ]] \
+&& wget -q https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip \
+&& unzip -o packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin \
+&& rm packer_${PACKER_VERSION}_linux_amd64.zip
+
+apt-get update
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
+apt-get upgrade -y
+apt-get autoremove -y
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+# clean up
+if [ ! ${REDHAT_BASED} ] ; then
+  apt-get clean
+fi
